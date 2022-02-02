839098d0784e1cfb584e92969e108084ec8b4e06
addisalem tafere <addisalem12@gmail.com>
2022-02-02 11:54:59 -0500

    filter added

--- Infrastructure/Persistance/Repository/PropertyRepository.cs
+++ Infrastructure/Persistance/Repository/PropertyRepository.cs
@@ -16,23 +16,83 @@ namespace Infrastructure.Persistance.Repository
         public async Task<IEnumerable<Property>> GetByTextAsync(string text, IEnumerable<string> marketKey, int limit)
         {
             var mgmtIndexName = CommonConfigurationsHelper.GetSettings().ElasticMgmtIndex;
-            var searchResponse = this._context.GetClient().Search<Property>(
-                     s => s.AllIndices()
-                         .Index(new[] { IndexName, mgmtIndexName })
-                         .IgnoreUnavailable().Size(limit)
-                       .Query(q => q.MultiMatch(
+            var nameList = new[] { "Abilene", "Abilene" };
+
+
+            QueryContainer query = new QueryContainerDescriptor<Property>();
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                query = query && new QueryContainerDescriptor<Property>().MultiMatch(
                                             m => m.Fields(fs => fs.Field(f => f.city)
                                                                   .Field(f => f.state)
                                                                   .Field(f => f.streetAddress))
                                             .Fuzziness(Fuzziness.Auto)
                                             .Lenient()
-                                            .Query(text))
-                                           && q.Bool(bq => bq
-                                     .Filter(fq => fq.Terms(t => t.Field(f => f.market).Terms(marketKey)))
+                                            .Query(text));
+            }
+            //if (marketKey.Any())
+            //{
+            //    query = query && new QueryContainerDescriptor<Property>()
+            //        .Bool(bq => bq.Filter(fq => fq.Terms(t => t.Field(f => f.market).Terms(new[] { "Abilene", "" }) )));
 
-                                            )));
 
-            return searchResponse.Documents;
+            //}
+            //var filters = new List<Func<QueryContainerDescriptor<Property>, QueryContainer>>();
+            //if (nameList.Any())
+            //{
+            //    //filters.Add(fq => fq.Terms(t => t.Field(f => f.market).Terms("San Francisco")));
+
+
+            //}
+            //var tagsFilter = marketKey.Select(value =>
+            //{
+            //    Func<QueryContainerDescriptor<Property>, QueryContainer> tagFilter = filter => filter
+            //        .Term(term => term
+            //            .Field(field => field.market)
+            //            .Value(value));
+
+            //    return tagFilter;
+            //});
+
+            //var result = _context.GetClient().Search<Property>(s => s
+            //   .Index(new[] { IndexName, mgmtIndexName }).
+            //   Query(_=>query));
+
+
+            var sd = new SearchDescriptor<Property>();
+            var qc = new QueryContainer();
+            var qd = new QueryContainerDescriptor<Property>();
+
+            sd.Size(limit);
+            sd.Index(new[] { IndexName, mgmtIndexName });
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                qc = qd.MultiMatch(m => m.Fields(fs => fs.Field(f => f.city)
+                      .Field(f => f.state)
+                      .Field(f => f.streetAddress))
+                        .Fuzziness(Fuzziness.Auto)
+                        .Lenient()
+                            .Query(text));
+
+            }
+
+            if (marketKey.Any())
+            {
+                qc = qd.Terms(m => m
+                    .Field(f => f.market)
+                    .Terms(marketKey.ToArray()));
+            }
+
+
+
+            sd.Query(q => q
+                 .Bool(b => b.Must(qc)));
+
+            var result = _context.GetClient().Search<Property>(s => s = sd);
+
+            return result.Documents;
         }
     }
 }
